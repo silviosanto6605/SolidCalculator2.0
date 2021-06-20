23c6a60fe2aeb22a32823c5bedc3d39973e56a22
Silvio Santoriello <silviosanto6605@gmail.com>
2021-06-20 20:12:01 +0200

    bug fix

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ const {
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences:{
+
+            devTools: false
+
+        }
+        
     })
 
     win.loadFile("index.html")
+    win.removeMenu()
 }
 app.whenReady().then(() => {
     createWindow()
+    
 })
