3a2556f8cdc9761d8f77680aa4591e5fc23f58d4
Silvio Santoriello <silviosanto6605@gmail.com>
2021-06-20 19:21:36 +0200

    add files

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function Cubo(){
+    window.location.href = "Cubo.html"
+}
+
+
+function Parallel(){
+    window.location.href = "Parallel.html"
+}
+
+
+function Piramide(){
+    window.location.href = "Piramide.html"
+}
+
+
+function Cilindro(){
+    window.location.href = "Cilindro.html"
+}
+
+/* da rivedere il codice js. 
+
+    Eliminare parti superflue.
+    Ottimizzare il codice
+
+*/
+function Sfera(){
+    window.location.href = "Sfera.html"
+}
